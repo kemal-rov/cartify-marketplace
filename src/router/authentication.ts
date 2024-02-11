be8a2abb2ff679491c511ce43ffd83b4fab9949d
Kemal - HN <krovcanin@harlemnext.com>
2024-02-11 16:38:27 +0100

    endpoint fix

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import { register } from '../controllers/authentication';
 
 export default (router: express.Router) => {
-    router.post('/auth.register', register);
+    router.post('/auth/register', register);
 };
