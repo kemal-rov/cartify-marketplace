962f15ddb2fe182ccfd49b75e7a1d5f89f000366
Kemal - HN <krovcanin@harlemnext.com>
2024-02-11 15:47:03 +0100

    fix updating user by id

--- db/users.ts
+++ db/users.ts
@@ -17,3 +17,8 @@ export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
     'authentication.sessionToken': sessionToken,
 });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values)
+    .save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
